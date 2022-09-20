f216b4fde09de04d6c212e8ac46b88074fc2fc51
Hillary Mongare <hillarymongare70@gmail.com>
2022-09-20 21:30:44 +0300

    added ready-made material ui components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
+import { TextInput } from "./components/TextInput";
+import { MessageLeft, MessageRight } from "./components/Message";
 
-function App() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      width: "80vw",
+      height: "80vh",
+      maxWidth: "500px",
+      maxHeight: "700px",
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      position: "relative",
+    },
+    paper2: {
+      width: "80vw",
+      maxWidth: "500px",
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      position: "relative",
+    },
+    container: {
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    messagesBody: {
+      width: "calc( 100% - 20px )",
+      margin: 10,
+      overflowY: "scroll",
+      height: "calc( 100% - 80px )",
+    },
+  })
+);
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className={classes.container}>
+      <Paper className={classes.paper} zDepth={2}>
+        <Paper id="style-1" className={classes.messagesBody}>
+          <MessageLeft
+            message="あめんぼあかいなあいうえお"
+            timestamp="MM/DD 00:00"
+            photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+            displayName=""
+            avatarDisp={true}
+          />
+          <MessageLeft
+            message="xxxxxhttps://yahoo.co.jp xxxxxxxxxあめんぼあかいなあいうえおあいうえおかきくけこさぼあかいなあいうえおあいうえおかきくけこさぼあかいなあいうえおあいうえおかきくけこさいすせそ"
+            timestamp="MM/DD 00:00"
+            photoURL=""
+            displayName="テスト"
+            avatarDisp={false}
+          />
+          <MessageRight
+            message="messageRあめんぼあかいなあいうえおあめんぼあかいなあいうえおあめんぼあかいなあいうえお"
+            timestamp="MM/DD 00:00"
+            photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+            displayName="まさりぶ"
+            avatarDisp={true}
+          />
+          <MessageRight
+            message="messageRあめんぼあかいなあいうえおあめんぼあかいなあいうえお"
+            timestamp="MM/DD 00:00"
+            photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+            displayName="まさりぶ"
+            avatarDisp={false}
+          />
+        </Paper>
+        <TextInput />
+      </Paper>
     </div>
   );
 }
-
-export default App;
